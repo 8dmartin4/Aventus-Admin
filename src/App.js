e60d8162eeacda604ee5fd65709d11b7f42a3e1c
Daniel Martin <8dmartin4@gmail.com>
2023-02-27 16:58:04 -0800

    added calendar

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Form from './scenes/form';
+import Calendar from './scenes/calendar';
 // import FAQ from './scenes/faq';
 // import Line from './scenes/line';
 // import Bar from './scenes/bar';
 // import Pie from './scenes/pie';
-// import Calendar from './scenes/calendar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -27,12 +27,12 @@ function App() {
               <Route path='/' element={<Dashboard/>}/>
               <Route path='/team' element={<Team/>}/>
               <Route path='/contacts' element={<Contacts/>}/>
+              <Route path='/form' element={<Form/>}/>
+              <Route path='/calendar' element={<Calendar/>}/>
               {/*<Route path='/faq' element={<FAQ/>}/>*/}
               {/*<Route path='/line' element={<Line/>}/>*/}
               {/*<Route path='/bar' element={<Bar/>}/>*/}
               {/*<Route path='/pie' element={<Pie/>}/>*/}
-              {/*<Route path='/calendar' element={<Calendar/>}/>*/}
-              <Route path='/form' element={<Form/>}/>
               {/*<Route path='/geography' element={<Geography/>}/>*/}
             </Routes>
           </main>
